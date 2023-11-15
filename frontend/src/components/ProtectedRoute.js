4c05b9fcb6fcd0fe233e94a5bca7006fd61ff9ce
tanya_simonova <tanya_simonova@mesto.ru-central1.internal>
2023-11-15 17:02:54 +0000

    hotfix/auto-test

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRouteElement = (props) => {
-  return props.loggedIn ? props.children : <Navigate to="/sign-in" replace />;
+  return props.loggedIn ? props.children : <Navigate to="/signin" replace />;
 };
 
 export default ProtectedRouteElement;
